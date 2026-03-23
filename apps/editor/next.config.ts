3f9f70a49496adef6c6cefc4a8dc0b34a08c53ce
marianos <sanchezmariano@outlook.com>
2026-03-23 19:55:20 +0100

    Editor 3d

--- apps/editor/next.config.ts
+++ apps/editor/next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from 'next'
 
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || ''
+
 const nextConfig: NextConfig = {
+  basePath,
+  output: 'standalone',
   devIndicators: false,
   typescript: {
     ignoreBuildErrors: true,
